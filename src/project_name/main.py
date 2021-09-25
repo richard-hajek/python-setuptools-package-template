0a599a15976e922d027cc148bdd578651fb6d0f2
Richard Hajek <richard.m.hajek@gmail.com>
2021-09-25 18:41:04 +0200

    feat: dynamic VERSION

--- src/project_name/main.py
+++ src/project_name/main.py
@@ -1,9 +1,10 @@
+import project_name
 from project_name import a
 from project_name.submodule import b
 
-
 def main():
     a.echo(b.add(1, 3))
+    print("Module version: " + project_name.VERSION)
 
 
 if __name__ == "__main__":
